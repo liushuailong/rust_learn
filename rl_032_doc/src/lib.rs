ee615281e737c6d84e9b0ac51a04e3cbd7b0a0aa
liushuailong <liushuailong@liushuailong.cn>
2022-01-20 21:45:45 +0800

    cargo doc learn from 令狐一冲“

---
+++ rl_032_doc/src/lib.rs
@@ -0,0 +1,15 @@
+//! My crate
+//!
+//! `my crate` is a cellection of utilites to make performing certion calculations more convinent.
+//!
+/// Add one to a number
+///
+/// # Example
+///
+/// ```
+/// let x = rl_032_doc::add_one(4);
+/// assert_eq!(5, x);
+/// ```
+pub fn add_one(x: i32) -> i32 {
+    x + 1
+}
